59c4f3b6b62d6314cab42d022b42b7ac513e9f32
Vatsal Gandhi <gandhivatsal17@gmail.com>
2026-03-19 11:44:47 +0530

    temp delete

--- lib/features/data/sources/remote/dashboard/dashboard_remote_ds.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_clean_architecture/features/domain/entities/voters/voter_entity.dart';
-
-abstract class DashboardRemoteDatasource {
-  Future<List<VoterEntity>> getVoters();
-}
